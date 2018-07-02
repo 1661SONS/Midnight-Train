13b7f509d47a362a752607e0999cc0bcbb266787
1661sons <julia.kerouac@gmail.com>
2018-07-02 15:30:03 -0400

    happy with styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+$(document).ready( function(){
+    
+    // need to hook in firebase
+    // need to hook in moment.js
+    // from form ..
+        // need to grab train name
+        // need to grab train destination
+        // need to grab first train time
+        // need to grab train frequency
+    // and save as variables
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // closing ready function
